edcdbb06264c77c8bf3c5181e35e6fda6701cee5
Trà Thanh Tú <tttrathanhtu2002@gmail.com>
2024-07-15 00:46:48 +0700

    update feature for product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,21 @@
+const { response } = require('express');
 const productModel = require('../models/productModel');
 
 const addProduct = async (req, res) => {
+  let products = await productModel.find({});
+  console.log('=================================', products);
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    console.log('last_array ================', last_product_array);
+    let last_product = last_product_array[0];
+    console.log('last_product==========', last_product);
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
   const product = new productModel({
-    id: req.body.id,
+    id: id,
     name: req.body.name,
     image: req.body.image,
     category: req.body.category,
@@ -18,4 +31,19 @@ const addProduct = async (req, res) => {
   });
 };
 
-module.exports = { addProduct };
+const removeProduct = async (req, res) => {
+  await productModel.findOneAndDelete({ id: req.body.id });
+  console.log('Removed');
+  res.json({
+    success: true,
+    name: req.body.name,
+  });
+};
+
+const showAllProducts = async (req, res) => {
+  let products = await productModel.find({});
+  console.log('=============== All Products Fetched');
+  res.send(products);
+};
+
+module.exports = { addProduct, removeProduct, showAllProducts };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { addProduct } = require('../controllers/productController');
+const {
+  addProduct,
+  removeProduct,
+  showAllProducts,
+} = require('../controllers/productController');
 
 router.post('/addproduct', addProduct);
+router.post('/removeproduct', removeProduct);
+router.get('/allproducts', showAllProducts);
 
 module.exports = router;
